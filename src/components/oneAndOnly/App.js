2eba470b98792ee31748a5775c919c3fe30b7181
CodeAgony <yaroslav.bkh@gmail.com>
2019-06-02 17:27:35 +0300

    Add a pointless http request

--- src/components/oneAndOnly/App.js
+++ src/components/oneAndOnly/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css'
+// Create new request
+const dataRequest = new XMLHttpRequest(); 
+dataRequest.open('GET', 'https://jsonplaceholder.typicode.com/users');
+// Send request
+dataRequest.send();
+// Log response as parsed object
+dataRequest.onload = function() {
+  let response = JSON.parse(dataRequest.responseText);
+console.log(response);
+}
+
+
 function App() {
   return (
     <div className="App">
       <h1 className="lol">Les do dis</h1>
+
     </div>
   );
 }
