c9d96f2462f99fe64dba47fab949f9054cf46d98
CodeAgony <yaroslav.bkh@gmail.com>
2019-06-05 21:16:37 +0300

    Use fetch API

--- src/components/oneAndOnly/App.js
+++ src/components/oneAndOnly/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './App.css'
-// Create new request
-const dataRequest = new XMLHttpRequest(); 
-dataRequest.open('GET', 'https://jsonplaceholder.typicode.com/users');
-// Send request
-dataRequest.send();
-// Log response as parsed object
-dataRequest.onload = function() {
-  let response = JSON.parse(dataRequest.responseText);
-console.log(response);
-}
+import React, {Component} from 'react';
+import './App.css';
 
+class App extends Component {
+// Fetch users on mount
+ componentDidMount() {
+    console.log("Mounted");
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => console.log(users));
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="lol">Les do dis</h1>
-
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1 className="lol">Les do dis</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
